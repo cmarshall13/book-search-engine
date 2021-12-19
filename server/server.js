ed8eb0884e70cc181851ae5560a5deb50b2f966d
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-12-19 13:55:55 -0500

    reinstalled apollo

--- server/server.js
+++ server/server.js
@@ -8,20 +8,14 @@ const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const startServer = async () => {
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
-  });
-  await server.start();
+});
+ 
 
-  server.applyMiddleware({ app });
-  console.log(`Use GQL at http://localhose:${PORT}${server.graphqlPath}`);
-};
-
-startServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,9 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
